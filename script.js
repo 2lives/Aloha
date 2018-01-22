5b3f4ea154126e822bd375ab1f5e643096db6cd2
Brandon Liu <brandon.slipjet@gmail.com>
2018-01-21 22:26:34 -0800

    near complete aloha

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+$("#emailbox").submit(function(event) { 
+  event.preventDefault();
+
+if (($("#enteremail").val() == "")){
+      alert("Please enter a valid email")
+
+} else {
+  alert("Thanks for Subscribing");
+
+    }
+  }
+);
+
+
+
 $('.main-carousel').flickity({
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  autoPlay: true,
+  pageDots: false
 });
+
+// $('#subbutton').on('click', function(event) {
+//   event.preventDefault();
+//   if($('#enteremail').val == ""){
+//       alert('Please enter a valid email');
+//   }
+// });
+
